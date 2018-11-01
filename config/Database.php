daa7bed44487759aa93bbdf6a84c3b38def16638
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2018-11-01 01:51:29 -0500

    Mostrar posts

---
+++ config/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database{
+
+    private $host       = 'localhost';
+    private $db_name    = 'api_binar10';
+    private $username   = 'root';
+    private $password   = '';
+    private $conn;
+
+    //Coneccion a la base de datos
+    public function connect()
+    {
+       $this->conn = null;
+
+        try{
+            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    } 
+
+}
